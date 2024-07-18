7c8575e5bd3c67593ddcad8ed5e74c80748d658c
Ravi chandra <githubravi01@gmail.com>
2024-07-18 10:21:28 +0530

    Complete SignUp

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -35,6 +35,19 @@ export async function POST(request: Request) {
     if (existingUserByEmail) {
       if (existingUserByEmail.isVerified) {
         // TODO: Write Here the concept
+        return Response.json(
+          {
+            success: false,
+            message: "Email is already taken",
+          },
+          { status: 400 }
+        );
+      } else {
+        const HashedPassword = await bcrypt(password, saltRounds);
+        existingUserByEmail.password = HashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpireAt = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
       }
     } else {
       // Create New User - Encrypting password by bcrypt
@@ -56,7 +69,23 @@ export async function POST(request: Request) {
       newUser.save();
     }
 
-    // TODO: Send The Verification Code
+    // TODO: Send The Verification Code - Use the SendVarification
+    const emailResponse = await SendVerification(email, username, verifyCode);
+
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 400 }
+      );
+    };
+
+    return Response.json({
+      success: true,
+      message: "User Created Successfully!",
+    }, {status : 200});
   } catch (error) {
     console.error("Error Registering User", error);
     return Response.json(
