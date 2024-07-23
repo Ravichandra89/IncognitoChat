2d52adc3ca2f987f172c93bf1ad952c9401d5be1
Ravi chandra <githubravi01@gmail.com>
2024-07-24 00:37:55 +0530

    SignIn Form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,116 @@
-'use client';
-import { useSession, signIn, signOut } from "next-auth/react";
+"use client";
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.username} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { toast, useToast } from "@/components/ui/use-toast";
+import { SignInSchema } from "@/Schema/SignIn";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { Sign } from "crypto";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { TypeOf } from "zod";
+
+export default function SignInForm() {
+  const router = useRouter();
+
+  // Zod for SignInSchema
+  const form = useForm<z.infer<typeof SignInSchema>>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const { toast } = useToast();
+
+  const onSubmit = async (data: z.infer<typeof SignInSchema>) => {
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+
+    if (result?.error) {
+      if (result.error === "CredentialsSignin") {
+        toast({
+          title: "Login Failed",
+          description: "Incorrect username or Password",
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: result.error,
+          variant: "destructive",
+        });
+      }
+    }
+
+    // After Sign-In Route to /dashboard
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+  };
+
   return (
-    <>
-      <h1>Not Login In  </h1>
-      <button className="bg-orange-500 px-3 py-2 rounded mt-4" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            WelCome To Application
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full" type="submit">
+              Sign In
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
