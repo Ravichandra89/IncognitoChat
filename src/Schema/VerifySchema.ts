1deef5afda2e00872fb8b0e84ec24602bcf5e5e3
Ravi chandra <githubravi01@gmail.com>
2024-07-24 01:43:49 +0530

    VerifySchema Debuging

--- src/Schema/VerifySchema.ts
+++ src/Schema/VerifySchema.ts
@@ -1,5 +1,5 @@
-import {z} from "zod";
+import { z } from "zod";
 
-export const verifyCode = z.object({
-    code : z.string().length(6, "Verify password length must be 6")
-})
+export const VerifySchema = z.object({
+  code: z.string().length(6, "Verify password length must be 6"),
+});
