f6b8234eb1733cfb7375b11514eff152f573db87
Ravi chandra <githubravi01@gmail.com>
2024-07-25 00:39:34 +0530

    User Dashboard Compeleted

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -17,7 +17,7 @@ import { messageAccept } from "@/Schema/AccecptMessage";
 
 function Dashboard() {
   const [messages, setMessages] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [IsLoading, setIsLoading] = useState(false);
   const [loadingSwitch, setLoadingSwitch] = useState(false);
 
   // Zod Resolver
@@ -33,7 +33,7 @@ function Dashboard() {
 
   // Get the status of User Accepting or not
   const fetchAcceptMessages = useCallback(async () => {
-    setLoading(true);
+    setIsLoading(true);
 
     try {
       const response = await axios.get<ApiResponse>("/api/accept-messages");
@@ -48,7 +48,7 @@ function Dashboard() {
         variant: "destructive",
       });
     } finally {
-      setLoading(false);
+      setIsLoading(false);
     }
   }, [setValue, toast]);
 
@@ -78,7 +78,7 @@ function Dashboard() {
   // Get Messages Function
   const fetchMessages = useCallback(
     async (refresh: boolean = false) => {
-      setLoading(true);
+      setIsLoading(true);
       setLoadingSwitch(true);
 
       try {
@@ -100,10 +100,109 @@ function Dashboard() {
           variant: "destructive",
         });
       } finally {
-        setLoading(false);
+        setIsLoading(false);
         setLoadingSwitch(false);
       }
     },
-    [setLoading, setLoadingSwitch, toast]
+    [setIsLoading, setLoadingSwitch, toast]
+  );
+
+  useEffect(() => {
+    if (!session || !session.user) return;
+
+    fetchMessages();
+  }, [session, setValue, toast, fetchAcceptMessages, fetchMessages]);
+
+  if (!session || !session.user) {
+    return <div></div>;
+  }
+
+  const { username } = session.user as User;
+  const baseUrl = `${window.location.protocol}//${window.location.host}`;
+  const profileUrl = `${baseUrl}/u/${username}`;
+
+  const copyclickBoard = () => {
+    navigator.clipboard.writeText(profileUrl);
+    toast({
+      title: "Url Copied",
+      description: "Copied Url To Clipboard",
+    });
+  };
+
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+
+  // FrontEnd part
+
+  return (
+    <div classgName="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy Your Link</h2>{" "}
+        <div>
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+          />
+
+          <Button onClick={copyclickBoard}>Copy</Button>
+        </div>
+      </div>
+
+      {/* Add Switch Here */}
+      <div className="mb-4">
+        <switch
+          {...register("acceptMessages")}
+          checked={acceptMessages}
+          onCheckedChange={handleAcceptMessages}
+          disable={loadingSwitch}
+        />
+
+        <span className="ml-2">
+          Accespt Messages : {acceptMessages ? "Yes" : "No"}
+        </span>
+      </div>
+
+      <Separator />
+
+      {/* Add the Refresh Button */}
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {IsLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
+
+      {/* Add the MessageCard Component  */}
+
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {messages.length > 0 ? (
+          messages.map((message, index) => {
+            <MessageCard
+              key={message._id}
+              message={message}
+              onMessageDelete={handleDeleteMessage}
+            />;
+          })
+        ) : (
+          <p>No messages to display.</p>
+        )}
+        ;
+      </div>
+    </div>
   );
 }
+
+export default Dashboard;
