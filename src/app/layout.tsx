1353124aa862255d865f493cb372bd0062b903dd
Ravi chandra <githubravi01@gmail.com>
2024-07-24 16:00:13 +0530

    Updated Layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={inter.className}>{children}
-        <Toaster />
+        <Navbar />
+        <body className={inter.className}>
+          {children}
+          <Toaster />
         </body>
       </AuthProvider>
     </html>
