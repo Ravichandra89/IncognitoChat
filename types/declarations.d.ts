e5881e2f7d9b9ffdd38731016988897b77b3499f
Ravi chandra <githubravi01@gmail.com>
2024-07-25 12:52:11 +0530

    AcetinityUi Intigration

---
+++ types/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module "tailwindcss/lib/util/flattenColorPalette" {
+  // You can define more specific types based on the actual usage if needed
+  const flattenColorPalette: (colors: any) => any;
+  export default flattenColorPalette;
+}
