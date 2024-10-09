bfe5d34fc64ef02dcf3c37b3c4d980a15f182255
Ravi chandra <134200599+Ravichandra89@users.noreply.github.com>
2024-10-09 12:18:33 +0530

    Delete types directory

--- types/ApiResponse.ts
+++
@@ -1,8 +0,0 @@
-import {Message} from "@/models/User";
-
-export interface ApiResponse{
-    success : boolean,
-    message: string,
-    isAcceptingMessage?: boolean,
-    messages?: Array <Message>
-}
